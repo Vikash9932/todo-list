2cb294a31c1a27894fea9a1e32a7a0d2f0a93616
vikash9932 <vikash9932@gmail.com>
2020-12-02 16:30:05 +0530

    Inside Build

---
+++ precache-manifest.a35cbe2496a90b20afde8675c1eaef8d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef0b3d542d88b76a7dcf86e7db32cde3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7cf675e8595f6ca582e",
+    "url": "/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "f3895c27ca3e8d405005",
+    "url": "/static/css/main.6e1c598c.chunk.css"
+  },
+  {
+    "revision": "e7cf675e8595f6ca582e",
+    "url": "/static/js/2.cdde882b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cdde882b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3895c27ca3e8d405005",
+    "url": "/static/js/main.d07967f3.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  },
+  {
+    "revision": "2719760172113d02d4f90892f5ef4ed3",
+    "url": "/static/media/cool-background.27197601.png"
+  }
+]);
